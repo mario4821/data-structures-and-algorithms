92fc0d376a7b3d662757b20735b03c40ab448fc7
Mario Flores Jr <m.flores4821@gmail.com>
2018-04-17 15:42:41 -0700

    added packages

--- reverse-an-array.js
+++ reverse-an-array.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function reverseArray (arr) {
-  let newArray = "";
-  for(let i = arr.length - 1; i >= 0; i--) {
+function reverseArray(arr) {
+  let newArray = '';
+  for (let i = arr.length - 1; i >= 0; i--) {
     newArray += arr[i];
   }
   return newArray;
