cadeaad748c749fb263a2d82d55253bbaa1cd5f6
Mario Flores Jr <m.flores4821@gmail.com>
2018-04-28 23:22:03 -0700

    code and test

--- largest-product.js
+++ largest-product.js
@@ -1,11 +1,41 @@
 'use strict';
 
-const largest = module.exports = {};
+function largestProduct(arr) {
+  let highValue = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      const currentArray = arr[i][j];
+    
+      if (arr[i - 1] !== undefined) {
+        const product = arr[i - 1][j] * currentArray;
+        if (product > highValue) {
+          highValue = product;
+        }
+      }
 
-largest.largestProduct (arr) {
-  for(let i = 0; i < arr.length; i++) {
-    for(let j = 0; j < arr.length; j++) {
+      if (arr[i + 1] !== undefined) {
+        const product = arr[i + 1][j] * currentArray;
+        if (product > highValue) {
+          highValue = product;
+        }
+      }
 
+      if (arr[i][j - 1] !== undefined) {
+        const product = arr[i][j - 1] * currentArray;
+        if (product > highValue) {
+          highValue = product;
+        }
+      } 
+
+      if (arr[i][j + 1] !== undefined) {
+        const product = arr[i][j + 1] * currentArray;
+        if (product > highValue) {
+          highValue = product;
+        }
+      }
     }
   }
-};
+  return highValue;
+}
+
+module.exports.largestProduct = largestProduct;
